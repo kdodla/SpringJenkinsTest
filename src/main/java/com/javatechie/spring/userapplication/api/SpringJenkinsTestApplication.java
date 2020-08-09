c6a8f705af0c0286304115cf6c7dc57cbd4a7ccf
Kalpana <kdodla1983@gmail.com>
2020-08-09 14:43:18 -0400

    first origin

---
+++ src/main/java/com/javatechie/spring/userapplication/api/SpringJenkinsTestApplication.java
@@ -0,0 +1,23 @@
+package com.javatechie.spring.userapplication.api;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsTestApplication {
+ Logger logger=LoggerFactory.getLogger(SpringJenkinsTestApplication.class);
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkinsTestApplication.class, args);
+	}
+	@PostConstruct
+	public void printData()
+	{
+		logger.info("I am in printData");
+		
+	}
+
+}
