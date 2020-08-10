88c34e4a9825465b3d08725d1692963aa4a1b671
Kalpana <kdodla1983@gmail.com>
2020-08-10 17:38:33 -0400

    added get method

--- src/main/java/com/javatechie/spring/userapplication/api/SpringJenkinsTestApplication.java
+++ src/main/java/com/javatechie/spring/userapplication/api/SpringJenkinsTestApplication.java
@@ -6,7 +6,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringJenkinsTestApplication {
  Logger logger=LoggerFactory.getLogger(SpringJenkinsTestApplication.class);
@@ -19,5 +22,9 @@ public class SpringJenkinsTestApplication {
 		logger.info("I am in printData");
 		logger.info("I am in printData");
 	}
-
+   @GetMapping("/hello")
+	public String hello()
+	{
+		return "Hello world";
+	}
 }
